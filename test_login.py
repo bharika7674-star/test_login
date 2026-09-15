caa8f09b9fd21355b3275c02537c19dadbce91b1
Harika <harikabavigadda@gmail.com>
2026-09-15 17:31:52 +0530

    Updated login test

--- test_login.py
+++ test_login.py
@@ -3,3 +3,6 @@ def login():
     print("Login test completed")
     print("completed login test")
     print("test incomplete login test")
+def test_login():
+    print("Login test started")
+    print("Login successful")
